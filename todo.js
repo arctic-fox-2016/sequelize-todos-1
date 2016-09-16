187b32d3908e34cd67e20e46c7bdd8739499710c
ivangerard <ivan.gerard@hotmail.com>
2016-09-16 11:42:29 +0700

    List done

--- todo.js
+++ todo.js
@@ -1,3 +1,8 @@
 'use strict';
 
 //write your code here
+
+
+model.Users.create({ first_name: 'Rani Grand',task:'Nyangkul Duid',status:'[]', updatedAt: new Date(),createdAt:new Date()}).then(function(task) {
+  // you can now access the newly created task via the variable task
+})
